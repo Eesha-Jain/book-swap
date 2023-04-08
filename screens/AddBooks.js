f34135a4a59bcb5949ea65c55121512bc4a0eac3
Eesha-Jain <eesha.jain801@gmail.com>
2023-04-07 22:06:36 -0700

    fix weird arrow thingys

--- screens/AddBooks.js
+++ screens/AddBooks.js
@@ -3,11 +3,7 @@ import { StyleSheet, View, Dimensions, Text, Image } from "react-native";
 
 const win = Dimensions.get("window");
 
-<<<<<<<< HEAD:screens/AddBooks.js
 function addBooks(props) {
-========
-function Preferences(props) {
->>>>>>>> 652edfba19a42bf309cd2259299c647a173fba11:screens/Preferences.js
   return <View style={styles.container}></View>;
 }
 
@@ -19,8 +15,4 @@ const styles = StyleSheet.create({
   },
 });
 
-<<<<<<<< HEAD:screens/AddBooks.js
 export default addBooks;
-========
-export default Preferences;
->>>>>>>> 652edfba19a42bf309cd2259299c647a173fba11:screens/Preferences.js
