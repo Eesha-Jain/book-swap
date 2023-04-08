64a4a6d03823e499ac0d35e549dd0a756a2e32ca
lightningstar425 <lightning427@gmail.com>
2023-04-07 18:01:42 -0700

    feat: Book type bottons added

--- screens/2Preferences.js
+++ screens/2Preferences.js
@@ -1,17 +1,53 @@
 import React, { Component } from "react";
-import { StyleSheet, View, Dimensions, Text, Image } from "react-native";
+import { StyleSheet, View, Dimensions, Text, Image, useState } from "react-native";
+
 
 const win = Dimensions.get("window");
 
 function Preferences(props) {
-  return <View style={styles.container}></View>;
+  const [nonFiction, fiction, textBook] = useState(false);
+// 
+  return <View style={styles.container}>
+<Text>Preferences</Text>
+<Text>Type of Books: </Text>
+<Button
+//Non-Fiction Preference
+        onPress={() => {
+          nonFiction(true);
+        }}
+        disabled={NonFiction}
+        title={NonFiction ? 'Non-Fiction Selected' : 'Non-Fiction'}
+      />
+
+<Button
+//Fiction Preference 
+        onPress={() => {
+          fiction(true);
+        }}
+        disabled={fiction}
+        title={fiction ? 'Fiction Selected' : 'Fiction'}
+      />
+
+<Button
+//TextBook Preference
+        onPress={() => {
+          textBook(true);
+        }}
+        disabled={textBook}
+        title={textBook ? 'Textbooks Selected' : 'Textbooks'}
+      />
+
+  </View>;
 }
 
 const styles = StyleSheet.create({
   container: {
+    
     alignItems: "center",
     backgroundColor: "white",
     height: win.height,
+     
+
   },
 });
 
