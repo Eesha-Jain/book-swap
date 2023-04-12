c7de1c5e1874617cd6ab28c37a889f1756ecd892
kmaanasar <125827551+kmaanasar@users.noreply.github.com>
2023-04-11 19:40:07 -0700

    Update DrawerItems.js

--- DrawerItems.js
+++ DrawerItems.js
@@ -1,1 +1,30 @@
-
+export default [ 
+  {
+    name: 'Profile', 
+    iconType: 'Ionicons', 
+    iconName: 'person-outline'
+  }, 
+  {
+    name: 'Library', 
+    iconType: 'Ionicons', 
+    iconName: 'library-outline'
+  }, 
+  { 
+    name: 'Messages', 
+    iconType: 'AntDesign', 
+    iconName: 'message1'
+  }, 
+  {
+    name: 'Find Books', 
+    iconType: 'AntDesign', 
+    iconName: 'search1'
+  }, 
+  {
+    name: 'Settings', 
+    iconType: 'Feather', 
+    iconName: 'settings'
+  }, 
+  {
+    name: 'Help', 
+    iconType: 'Feather', 
+    iconName: 'help-circle'
