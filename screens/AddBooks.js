2ab758736011c03239f67afecab24c3f967895fb
Megan Pereira <pmegan883@gmail.com>
2023-04-13 22:17:39 -0700

    Added code for add books through React Native

--- screens/AddBooks.js
+++ screens/AddBooks.js
@@ -16,3 +16,73 @@ const styles = StyleSheet.create({
 });
 
 export default addBooks;
+
+
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+
+export default function App() {
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+  const [condition, setCondition] = useState('');
+
+  const handleAddBook = () => {
+    // Here you can add the logic to save the book information to a bookshelf or database
+    console.log(`Added book: ${title} by ${author} (${condition})`);
+    // Clear the inputs after adding the book
+    setTitle('');
+    setAuthor('');
+    setCondition('');
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Add a new book to your bookshelf</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Title"
+        value={title}
+        onChangeText={setTitle}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Author"
+        value={author}
+        onChangeText={setAuthor}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Condition"
+        value={condition}
+        onChangeText={setCondition}
+      />
+      <Button
+        title="Add book"
+        onPress={handleAddBook}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 20,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 4,
+    padding: 10,
+    marginBottom: 10,
+    width: '100%',
+  },
+});
