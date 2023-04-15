92221c80b04ea8c7c68cc13930cc2bcbc2c3d5d9
kmaanasar <125827551+kmaanasar@users.noreply.github.com>
2023-04-15 10:50:24 -0700

    Update AddBooks.js

--- screens/AddBooks.js
+++ screens/AddBooks.js
@@ -1,5 +1,6 @@
-import React, { useState } from "react";
-import { StyleSheet, Text, View, TextInput, Button } from "react-native";
+import React, { FC, useState } from "react";
+import { StyleSheet, Text, TouchableOpacity, View, TextInput, Button } from "react-native";
+import { Icon } from 'react-native-elements';
 
 export default function App() {
   const [title, setTitle] = useState("");
@@ -30,11 +31,61 @@ export default function App() {
         value={author}
         onChangeText={setAuthor}
       />
-      <TextInput
-        style={styles.input}
-        placeholder="Condition"
-        value={condition}
-        onChangeText={setCondition}
+      interface Props {
+  label: string;
+}
+
+const Dropdown: FC<Props> = ({ label }) => {
+  const [visible, setVisible] = useState(false);
+
+  const toggleDropdown = () => {
+    setVisible(!visible);
+  };
+
+  const renderDropdown = () => {
+    if (visible) {
+      return (
+          <Text style={styles.dropdown}>
+            This is where the dropdown will be rendered.
+          </Text>
+      );
+    }
+  };
+
+  return (
+    <TouchableOpacity
+      style={styles.button}
+      onPress={toggleDropdown}
+    >
+      {renderDropdown()}
+      <Text style={styles.buttonText}>{label}</Text>
+      <Icon type='font-awesome' name='chevron-down'/>
+    </TouchableOpacity>
+  );
+}
+
+const styles = StyleSheet.create({
+  button: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: '#efefef',
+    height: 50,
+    width: '90%',
+    paddingHorizontal: 10,
+    zIndex: 1,
+  },
+  buttonText: {
+    flex: 1,
+    textAlign: 'center',
+  },
+  dropdown: {
+    position: 'absolute',
+    backgroundColor: '#fff',
+    top: 50,
+  },
+});
+
+export default Dropdown;
       />
       <Button title="Add Book" onPress={handleAddBook} />
     </View>
