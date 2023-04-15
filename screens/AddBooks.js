fa6fdf6b63aa1c98ca92b20bbbc0cb4d6a8fc265
kmaanasar <125827551+kmaanasar@users.noreply.github.com>
2023-04-15 11:15:24 -0700

    Update AddBooks.js

--- screens/AddBooks.js
+++ screens/AddBooks.js
@@ -55,10 +55,98 @@ export default function App() {
       />
       <TouchableOpacity style={styles.button} onPress={toggleDropdown}>
         {renderDropdown()}
-        <Text style={styles.buttonText}>Test</Text>
+        <Text style={styles.buttonText}>Condition</Text>
         <Icon type="font-awesome" name="chevron-down" />
       </TouchableOpacity>
       <Button title="Add Book" onPress={handleAddBook} />
+interface Props {
+  label: string;
+  data: Array<{ label: string; value: string }>;
+  onSelect: (item: { label: string; value: string }) => void;
+  const renderDropdown = (): ReactElement<any, any> => {
+    return (
+      <Modal visible={visible} transparent animationType="none">
+        <TouchableOpacity
+          style={styles.overlay}
+          onPress={() => setVisible(false)}
+        >
+          <View style={[styles.dropdown, { top: dropdownTop }]}>
+            <FlatList
+              data={data}
+              renderItem={renderItem}
+              keyExtractor={(item, index) => index.toString()}
+            />
+          </View>
+        </TouchableOpacity>
+      </Modal>
+dropdown: {
+    position: 'absolute',
+    backgroundColor: '#fff',
+    width: '100%',
+    shadowColor: '#000000',
+    shadowRadius: 4,
+    shadowOffset: { height: 4, width: 0 },
+    shadowOpacity: 0.5,
+const DropdownButton = useRef();
+const [dropdownTop, setDropdownTop] = useState(0);
+  <TouchableOpacity
+  ref={DropdownButton}
+  style={styles.button}
+  onPress={toggleDropdown}
+>
+    const toggleDropdown = (): void => {
+  visible ? setVisible(false) : openDropdown();
+};
+
+const openDropdown = (): void => {
+  DropdownButton.current.measure((_fx, _fy, _w, h, _px, py) => {
+    setDropdownTop(py + h);
+  });
+  setVisible(true);
+};
+  const renderItem = ({ item }): ReactElement<any, any> => (
+  <TouchableOpacity style={styles.item} onPress={() => onItemPress(item)}>
+    <Text>{item.label}</Text>
+  </TouchableOpacity>
+);
+const onItemPress = (item): void => {
+    setSelected(item);
+    onSelect(item);
+    setVisible(false);
+  };
+const [selected, setSelected] = useState(undefined);
+<Text style={styles.buttonText}>
+  {(selected && selected.label) || label}
+</Text>
+item: {
+  paddingHorizontal: 10,
+  paddingVertical: 10,
+  borderBottomWidth: 1,
+},
+  const App: FC = () => {
+  const [selected, setSelected] = useState(undefined);
+  const data = [
+    { label: 'Damaged', value: '1' },
+    { label: 'Used', value: '2' },
+    { label: 'Perfect', value: '3' },
+  ];
+
+  return (
+    <View style={styles.container}>
+      {!!selected && (
+        <Text>
+          Selected: label = {selected.label} and value = {selected.value}
+        </Text>
+      )}
+      <Dropdown label="Select Item" data={data} onSelect={setSelected} />
+      <Text>This is the rest of the form.</Text>
+    </View>
+  );
+};
+  },
+    );
+  };
+}
     </View>
   );
 }
