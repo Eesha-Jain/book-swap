6712df2295b160b3f51a92ff6e7b650f035c504a
Eesha-Jain <eesha.jain801@gmail.com>
2023-04-15 11:34:19 -0700

    dropdown

--- screens/AddBooks.js
+++ screens/AddBooks.js
@@ -2,35 +2,28 @@ import React, { FC, useState } from "react";
 import {
   StyleSheet,
   Text,
-  TouchableOpacity,
   Pressable,
   View,
   TextInput,
   Button,
 } from "react-native";
-import { Icon } from "react-native-elements";
+import Dropdown from "../components/Dropdown";
 import * as ImagePicker from "expo-image-picker";
 
 export default function App({ navigation: { navigate } }) {
   const [title, setTitle] = useState("");
   const [author, setAuthor] = useState("");
   const [condition, setCondition] = useState("");
-  const [visible, setVisible] = useState(false);
   const [image, setImage] = useState(null);
 
-  const toggleDropdown = () => {
-    setVisible(!visible);
-  };
-
-  const renderDropdown = () => {
-    if (visible) {
-      return (
-        <Text style={styles.dropdown}>
-          This is where the dropdown will be rendered.
-        </Text>
-      );
-    }
-  };
+  const [selected, setSelected] = useState(undefined);
+  const data = [
+    { label: "One", value: "1" },
+    { label: "Two", value: "2" },
+    { label: "Three", value: "3" },
+    { label: "Four", value: "4" },
+    { label: "Five", value: "5" },
+  ];
 
   const handleAddBook = () => {
     // Here you can add the logic to save the book information to a bookshelf or database
@@ -73,8 +66,9 @@ export default function App({ navigation: { navigate } }) {
         value={author}
         onChangeText={setAuthor}
       />
+      <Dropdown label="Select Item" data={data} onSelect={setSelected} />
       <Button
-        style={{ color: "#D29B0C" }}
+        style={{ color: "#D29B0C", marginTop: 20 }}
         title="Take Picture of Book"
         onPress={pickImage}
       />
@@ -96,123 +90,11 @@ export default function App({ navigation: { navigate } }) {
           Add Book
         </Text>
       </Pressable>
-      <TouchableOpacity style={styles.button} onPress={toggleDropdown}>
-        {renderDropdown()}
-        <Text style={styles.buttonText}>Condition</Text>
-        <Icon type="font-awesome" name="chevron-down" />
-      </TouchableOpacity>
-      <Button title="Add Book" onPress={handleAddBook} />
-interface Props {
-  label: string;
-  data: Array<{ label: string; value: string }>;
-  onSelect: (item: { label: string; value: string }) => void;
-  const renderDropdown = (): ReactElement<any, any> => {
-    return (
-      <Modal visible={visible} transparent animationType="none">
-        <TouchableOpacity
-          style={styles.overlay}
-          onPress={() => setVisible(false)}
-        >
-          <View style={[styles.dropdown, { top: dropdownTop }]}>
-            <FlatList
-              data={data}
-              renderItem={renderItem}
-              keyExtractor={(item, index) => index.toString()}
-            />
-          </View>
-        </TouchableOpacity>
-      </Modal>
-dropdown: {
-    position: 'absolute',
-    backgroundColor: '#fff',
-    width: '100%',
-    shadowColor: '#000000',
-    shadowRadius: 4,
-    shadowOffset: { height: 4, width: 0 },
-    shadowOpacity: 0.5,
-const DropdownButton = useRef();
-const [dropdownTop, setDropdownTop] = useState(0);
-  <TouchableOpacity
-  ref={DropdownButton}
-  style={styles.button}
-  onPress={toggleDropdown}
->
-    const toggleDropdown = (): void => {
-  visible ? setVisible(false) : openDropdown();
-};
-
-const openDropdown = (): void => {
-  DropdownButton.current.measure((_fx, _fy, _w, h, _px, py) => {
-    setDropdownTop(py + h);
-  });
-  setVisible(true);
-};
-  const renderItem = ({ item }): ReactElement<any, any> => (
-  <TouchableOpacity style={styles.item} onPress={() => onItemPress(item)}>
-    <Text>{item.label}</Text>
-  </TouchableOpacity>
-);
-const onItemPress = (item): void => {
-    setSelected(item);
-    onSelect(item);
-    setVisible(false);
-  };
-const [selected, setSelected] = useState(undefined);
-<Text style={styles.buttonText}>
-  {(selected && selected.label) || label}
-</Text>
-item: {
-  paddingHorizontal: 10,
-  paddingVertical: 10,
-  borderBottomWidth: 1,
-},
-  const App: FC = () => {
-  const [selected, setSelected] = useState(undefined);
-  const data = [
-    { label: 'Damaged', value: '1' },
-    { label: 'Used', value: '2' },
-    { label: 'Perfect', value: '3' },
-  ];
-
-  return (
-    <View style={styles.container}>
-      {!!selected && (
-        <Text>
-          Selected: label = {selected.label} and value = {selected.value}
-        </Text>
-      )}
-      <Dropdown label="Select Item" data={data} onSelect={setSelected} />
-      <Text>This is the rest of the form.</Text>
-    </View>
-  );
-};
-  },
-    );
-  };
-}
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  button: {
-    flexDirection: "row",
-    alignItems: "center",
-    backgroundColor: "#efefef",
-    height: 50,
-    width: "90%",
-    paddingHorizontal: 10,
-    zIndex: 1,
-  },
-  buttonText: {
-    flex: 1,
-    textAlign: "center",
-  },
-  dropdown: {
-    position: "absolute",
-    backgroundColor: "#fff",
-    top: 50,
-  },
   container: {
     flex: 1,
     backgroundColor: "#fff",
